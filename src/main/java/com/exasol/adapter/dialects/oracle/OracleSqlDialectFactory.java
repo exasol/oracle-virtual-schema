d650cf9eece59d49fc10edf8cc58906d249ec455
Marcelo Chiaradia <chiaradiamarcelo@gmail.com>
2021-01-11 18:27:12 +0100

    #1: Migrate Oracle virtual schema to its own repository

---
+++ src/main/java/com/exasol/adapter/dialects/oracle/OracleSqlDialectFactory.java
@@ -0,0 +1,29 @@
+package com.exasol.adapter.dialects.oracle;
+
+import com.exasol.adapter.AdapterProperties;
+import com.exasol.adapter.dialects.SqlDialect;
+import com.exasol.adapter.dialects.SqlDialectFactory;
+import com.exasol.adapter.jdbc.ConnectionFactory;
+import com.exasol.logging.VersionCollector;
+
+/**
+ * Factory for the Oracle SQL dialect.
+ */
+public class OracleSqlDialectFactory implements SqlDialectFactory {
+    @Override
+    public String getSqlDialectName() {
+        return OracleSqlDialect.NAME;
+    }
+
+    @Override
+    public SqlDialect createSqlDialect(final ConnectionFactory connectionFactory, final AdapterProperties properties) {
+        return new OracleSqlDialect(connectionFactory, properties);
+    }
+    
+    @Override
+	public String getSqlDialectVersion() {
+		final VersionCollector versionCollector = new VersionCollector(
+                "META-INF/maven/com.exasol/mysql-virtual-schema/pom.properties");
+        return versionCollector.getVersionNumber();
+	}
+}
