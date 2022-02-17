50f0e4455049d0e049ba33cff335205fce7bc57d
Pieterjan Spoelders <pj.spoelders@gmail.com>
2022-02-17 15:06:22 +0100

    comments + import

--- src/test/java/com/exasol/adapter/dialects/oracle/OracleContainerDBA.java
+++ src/test/java/com/exasol/adapter/dialects/oracle/OracleContainerDBA.java
@@ -2,12 +2,15 @@ package com.exasol.adapter.dialects.oracle;
 
 import java.sql.*;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.utility.DockerImageName;
 
-//reason for this:
-//rights:
+//reason for creating this class on top of oracle container is because this one,
+// differently from the other ones (postgres, mysql, gives back a non-privileged user connection,
+// I've added a method to this class that returns a dba connection .
+//more info in the links below:
 //https://github.com/testcontainers/testcontainers-java/issues/4615
 //https://github.com/gvenzl/oci-oracle-xe/issues/41
 
@@ -37,8 +40,6 @@ public class OracleContainerDBA extends OracleContainer {
 
             while ((System.currentTimeMillis() < (start + TimeUnit.SECONDS.toMillis(this.connectTimeoutSeconds))) && this.isRunning()) {
                 try {
-                    // this.logger().debug("Trying to create JDBC connection using {} to {} with properties: {}", new
-                    // Object[]{this.driver.getClass().getName(), url, info});
                     return jdbcDriverInstance.connect(url, info);
                 } catch (final SQLException exception) {
                     lastException = exception;
