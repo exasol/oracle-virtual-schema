51979c17de75f38fc71bd37963ae610193805de4
KK <christoph.kuhnke@exasol.com>
2022-09-28 14:05:09 +0200

    #23: Updated to VSCJDBC 10.0.1 and fixed vulnerabilities.

--- src/test/java/com/exasol/adapter/dialects/oracle/OracleQueryRewriterTest.java
+++ src/test/java/com/exasol/adapter/dialects/oracle/OracleQueryRewriterTest.java
@@ -4,18 +4,13 @@ import static com.exasol.adapter.dialects.oracle.OracleProperties.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.util.Map;
+import java.sql.*;
+import java.util.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.exasol.adapter.AdapterException;
@@ -23,10 +18,13 @@ import com.exasol.adapter.AdapterProperties;
 import com.exasol.adapter.dialects.*;
 import com.exasol.adapter.dialects.rewriting.AbstractQueryRewriterTestBase;
 import com.exasol.adapter.jdbc.ConnectionFactory;
+import com.exasol.adapter.metadata.DataType;
 import com.exasol.adapter.sql.TestSqlStatementFactory;
 
 @ExtendWith(MockitoExtension.class)
 public class OracleQueryRewriterTest extends AbstractQueryRewriterTestBase {
+    private static final List<DataType> EMPTY_SELECT_LIST_DATA_TYPES = Collections.emptyList();
+
     @BeforeEach
     void beforeEach() {
         this.statement = TestSqlStatementFactory.createSelectOneFromDual();
@@ -40,33 +38,38 @@ public class OracleQueryRewriterTest extends AbstractQueryRewriterTestBase {
                 ORACLE_CONNECTION_NAME_PROPERTY, "ora_connection"));
         final SqlDialectFactory dialectFactory = new OracleSqlDialectFactory();
         final SqlDialect dialect = dialectFactory.createSqlDialect(connectionFactoryMock, properties);
-        final QueryRewriter queryRewriter = new OracleQueryRewriter(dialect, null,connectionFactoryMock.getConnection(),properties);
-        assertThat(queryRewriter.rewrite(this.statement, EXA_METADATA, properties),
+        final QueryRewriter queryRewriter = new OracleQueryRewriter(dialect, null,
+                connectionFactoryMock.getConnection(), properties);
+        assertThat(queryRewriter.rewrite(this.statement, EMPTY_SELECT_LIST_DATA_TYPES, EXA_METADATA, properties),
                 equalTo("IMPORT FROM ORA AT ora_connection STATEMENT 'SELECT TO_CHAR(1) FROM \"DUAL\"'"));
     }
+
     @Test
     void testRewriteToImportFromOraWithConnectionDetailsInPropertiesJDBCDatatypeMapping(
             @Mock final ConnectionFactory connectionFactoryMock) throws AdapterException, SQLException {
         final AdapterProperties properties = new AdapterProperties(Map.of( //
                 ORACLE_IMPORT_PROPERTY, "true", //
-                ORACLE_CONNECTION_NAME_PROPERTY, "ora_connection",
-                GENERATE_JDBC_DATATYPE_MAPPING_FOR_OCI_PROPERTY, "true"));
+                ORACLE_CONNECTION_NAME_PROPERTY, "ora_connection", GENERATE_JDBC_DATATYPE_MAPPING_FOR_OCI_PROPERTY,
+                "true"));
         final SqlDialectFactory dialectFactory = new OracleSqlDialectFactory();
         final SqlDialect dialect = dialectFactory.createSqlDialect(connectionFactoryMock, properties);
-        var oracleMetadataReader = new OracleMetadataReader(mockConnection(), properties);
-        final QueryRewriter queryRewriter = new OracleQueryRewriter(dialect, oracleMetadataReader,mockConnection(),properties);
-        assertThat(queryRewriter.rewrite(this.statement, EXA_METADATA, properties),
+        final var oracleMetadataReader = new OracleMetadataReader(mockConnection(), properties);
+        final QueryRewriter queryRewriter = new OracleQueryRewriter(dialect, oracleMetadataReader, mockConnection(),
+                properties);
+        assertThat(queryRewriter.rewrite(this.statement, EMPTY_SELECT_LIST_DATA_TYPES, EXA_METADATA, properties),
                 equalTo("IMPORT INTO (c1 DECIMAL(18, 0)) FROM ORA AT ora_connection STATEMENT 'SELECT TO_CHAR(1) FROM \"DUAL\"'"));
     }
+
     @Override
     protected Connection mockConnection() throws SQLException {
-        ResultSetMetaData metadataMock = (ResultSetMetaData) Mockito.mock(ResultSetMetaData.class);
+        final ResultSetMetaData metadataMock = Mockito.mock(ResultSetMetaData.class);
         Mockito.lenient().when(metadataMock.getColumnCount()).thenReturn(1);
         Mockito.lenient().when(metadataMock.getColumnType(1)).thenReturn(4);
-        PreparedStatement statementMock = (PreparedStatement)Mockito.mock(PreparedStatement.class);
+        final PreparedStatement statementMock = Mockito.mock(PreparedStatement.class);
         Mockito.lenient().when(statementMock.getMetaData()).thenReturn(metadataMock);
-        Connection connectionMock = (Connection)Mockito.mock(Connection.class);
-        Mockito.lenient().when(connectionMock.prepareStatement((String) ArgumentMatchers.any())).thenReturn(statementMock);
+        final Connection connectionMock = Mockito.mock(Connection.class);
+        Mockito.lenient().when(connectionMock.prepareStatement((String) ArgumentMatchers.any()))
+                .thenReturn(statementMock);
         return connectionMock;
     }
 }
