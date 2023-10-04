7b0a1b9da90a9c6cce6c7341dcab1c674625461c
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-10-04 06:49:56 +0200

    Downgrade Exasol version to debug startup issues

--- src/test/java/com/exasol/adapter/dialects/oracle/OracleSqlDialectIT.java
+++ src/test/java/com/exasol/adapter/dialects/oracle/OracleSqlDialectIT.java
@@ -57,7 +57,8 @@ class OracleSqlDialectIT {
     private static final String TABLE_ORACLE_TIMESTAMPS = "TABLE_ORACLE_TIMESTAMPS";
 
     @Container
-    private static final ExasolContainer<? extends ExasolContainer<?>> exasolContainer = new ExasolContainer<>() //
+    private static final ExasolContainer<? extends ExasolContainer<?>> exasolContainer = new ExasolContainer<>(
+            EXASOL_DOCKER_IMAGE_REFERENCE) //
             .withRequiredServices(ExasolService.BUCKETFS, ExasolService.UDF).withReuse(true);
     @Container
     private static final OracleContainerDBA oracleContainer = new OracleContainerDBA(ORACLE_CONTAINER_NAME);
