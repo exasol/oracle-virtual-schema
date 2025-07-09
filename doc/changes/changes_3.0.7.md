e1e095d6b896821a708d91f6f308685cb5da8843
mykhailo.skliar <mykhailo.skliar@exasol.com>
2025-07-09 20:58:06 +0200

    - updated project version and generated code with proejct-keeper:fix

---
+++ doc/changes/changes_3.0.7.md
@@ -0,0 +1,10 @@
+# Oracle Virtual Schema 3.0.7, released 2025-??-??
+
+Code name:
+
+## Summary
+
+## Features
+
+* ISSUE_NUMBER: description
+
--- src/test/java/com/exasol/adapter/dialects/oracle/helper/ThrowsSqlConsumer.java
+++ src/test/java/com/exasol/adapter/dialects/oracle/helper/ThrowsSqlConsumer.java
@@ -3,7 +3,7 @@ package com.exasol.adapter.dialects.oracle.helper;
 import java.sql.SQLException;
 
 /**
- * Functional interface, which extends {@link java.util.function.Consumer} and provides acceptThrowable method, which delegates to accept and throws SQLException
+ * Functional interface, which is similar to {@link java.util.function.Consumer} and throws SQLException
  */
 @FunctionalInterface
 public interface ThrowsSqlConsumer<T> {
